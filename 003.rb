6c72ec9ba76f217720b76be4ea348295713faa4c
homepage <homepage@prd-scripts.(none)>
2012-08-06 20:26:49 +0900

    #3 delete MakePrime as it no use

--- 003.rb
+++ 003.rb
@@ -1,19 +1,3 @@
-def MakePrime(max)
-	prime = []
-	2.upto(max) do |n|
-		prime.push(n)
-	end
-
-	max.times do |i|
-		prime.delete_if { |n| (n % prime[i] == 0) && (n != prime[i]) }
-		break unless prime[i+1]
-		break if prime[i] > (max/2)
-	end
-
-	#p prime
-	return prime
-end
-
 def factrization(num)
 	max = num
 	while num != 1  do
